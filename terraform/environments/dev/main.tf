4c723d7c0bb09035a8c20bde836d38140db437b4
Lokesh Kumar Jain <lokeshjain2008@gmail.com>
2024-12-26 18:08:23 +0530

    Update AWS region to ap-south-1

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -7,7 +7,7 @@ module "vpc" {
 
   vpc_name = "darpo-dev"
   vpc_cidr = "10.0.0.0/16"
-  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  availability_zones = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
   private_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnet_cidrs  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   environment = "dev"
--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -7,7 +7,7 @@ module "vpc" {
 
   vpc_name = "darpo-prod"
   vpc_cidr = "10.1.0.0/16"
-  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  availability_zones = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
   private_subnet_cidrs = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
   public_subnet_cidrs  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
   environment = "prod"
