20e9caeaf4d50e691011f4bd6ba69944d5056bdc
Lokesh Kumar Jain <lokeshjain2008@gmail.com>
2024-12-26 23:23:07 +0530

    Fix EKS module configuration and add required variables

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -2,25 +2,32 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.28"
+  cluster_name                   = var.cluster_name
+  cluster_version                = "1.28"
+  cluster_endpoint_public_access = true
 
   vpc_id     = var.vpc_id
   subnet_ids = var.subnet_ids
 
   eks_managed_node_groups = {
-    main = {
-      desired_size = var.desired_size
+    default = {
+      name = "${var.cluster_name}-ng"
+
       min_size     = var.min_size
       max_size     = var.max_size
+      desired_size = var.desired_size
 
       instance_types = var.instance_types
       capacity_type  = var.environment == "prod" ? "ON_DEMAND" : "SPOT"
+
+      tags = {
+        Environment = var.environment
+      }
     }
   }
 
   tags = {
     Environment = var.environment
-    Project     = "darpo"
+    Terraform   = "true"
   }
 }
---
+++ terraform/modules/eks/variables.tf
@@ -0,0 +1,39 @@
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where EKS cluster will be created"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for EKS cluster"
+  type        = list(string)
+}
+
+variable "environment" {
+  description = "Environment (dev/prod)"
+  type        = string
+}
+
+variable "desired_size" {
+  description = "Desired number of worker nodes"
+  type        = number
+}
+
+variable "min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+}
+
+variable "max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+}
+
+variable "instance_types" {
+  description = "List of EC2 instance types for worker nodes"
+  type        = list(string)
+}
