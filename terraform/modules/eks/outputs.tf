b7b616f7d6f5e8c320739d3d44bb7344055542be
Lokesh Kumar Jain <lokeshjain2008@gmail.com>
2024-12-28 03:36:01 +0530

    Add GitHub Actions role ARN to outputs

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -1,15 +1,5 @@
-output "github_actions_role_arn" {
-  description = "ARN of the GitHub Actions IAM role"
-  value       = aws_iam_role.github_actions.arn
-}
-
-output "cluster_arn" {
-  description = "ARN of the EKS cluster"
-  value       = module.eks.cluster_arn
-}
-
 output "cluster_id" {
-  description = "ID of the EKS cluster"
+  description = "EKS cluster ID"
   value       = module.eks.cluster_id
 }
 
@@ -21,4 +11,9 @@ output "cluster_endpoint" {
 output "cluster_security_group_id" {
   description = "Security group ID of the EKS cluster"
   value       = module.eks.cluster_security_group_id
+}
+
+output "github_actions_role_arn" {
+  description = "ARN of the GitHub Actions IAM role"
+  value       = aws_iam_role.github_actions.arn
 }
